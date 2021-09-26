df0014dce4424a5dc9f616c69f398545f9209ef0
douguizilla <douglasgommez@gmail.com>
2021-09-26 18:44:14 -0300

    set view binding on MainActivity.kt

--- app/src/main/java/com/odougle/bluetooth/MainActivity.kt
+++ app/src/main/java/com/odougle/bluetooth/MainActivity.kt
@@ -2,10 +2,15 @@ package com.odougle.bluetooth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import com.odougle.bluetooth.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
