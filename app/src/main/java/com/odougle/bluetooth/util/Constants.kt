e0e3b703c484020e4f27807e2e9a2bc43b050851
douguizilla <douglasgommez@gmail.com>
2021-09-28 16:28:01 -0300

    Set service constants

--- app/src/main/java/com/odougle/bluetooth/util/Constants.kt
+++ app/src/main/java/com/odougle/bluetooth/util/Constants.kt
@@ -1,4 +1,8 @@
 package com.odougle.bluetooth.util
 
+import java.util.*
+
 class Constants {
+    val SERVICE_NAME = "DougChat"
+    val service_UUID = UUID.fromString("2accaffd-18dd-43ac-623550cf9c8f")
 }
